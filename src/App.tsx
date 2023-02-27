ae4e4465ffd76520fdcd28e021bfa22f61993fee
PariharInder <2002inderjitsingh@gmail.com>
2023-02-27 15:18:25 -0500

    created ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import { useState } from 'react'
+import logo from './assets/logo.png'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <div>
+      <img src={logo} alt="Currency Converter Logo" />
+      <h2>Currency Converter</h2>
+      </div>
+      <div>
+        <p>Convert:</p>
+        <input type="number" name="amount" id="amount" />
+        <select name="currency-1" id="currency-1"></select>
+      </div>
+      <div>
+        <p>To:</p>
+        <select name="currency-1" id="currency-1"></select>
+      </div>
+      <div>
+        <h2>Result : </h2>
+      </div>
     </div>
   )
 }
